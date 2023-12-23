b64851cfc54a83f55cdcacffee6fde7a82c6413a
DEV-RAPHAEL <uniqueinternet2020@gmail.com>
2023-12-23 20:24:23 +0100

    Header Created

---
+++ components/Header.tsx
@@ -0,0 +1,49 @@
+"use client"
+import Image from 'next/image'
+import React from 'react'
+import {MagnifyingGlassIcon, UserCircleIcon} from "@heroicons/react/24/solid"
+import Avatar from "react-avatar"
+
+function Header() {
+  return (
+    <div>
+      <header>
+        <div className='flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2x1'>
+                <Image 
+                src="/logo.png"
+                alt='Logo'
+                width={300}
+                height={100}
+                className='w-44 md:w-56 pb-10 md:pb-0 object-contain'
+                /> 
+                  <div className='flex items-center space-x-5 justify-end w-full'>
+                      {/* Search */}
+                      <form action=""
+                      className='flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial'
+                      >
+                        <MagnifyingGlassIcon 
+                        className="h-6 w-6 text-gray-400"
+                        />
+                        <input type="text" placeholder='Search' className='flex-1 outline-none p-2'/>
+                        <button hidden type='submit' >Search</button>
+                      </form>
+                      {/* Avatar */}
+                      <Avatar name="Ajayi Raphael" round color="blue" size="50" />
+                      </div>
+          </div>
+
+          <div className='flex items-center justify-center px-5 md:py-5'>
+          <p className="flex items-center textt-sm font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055d1]">
+          <UserCircleIcon
+          className="inline-block h-10 w-10 text-[#0055D1] mr-1"
+          />   
+          Summarizing tasks, please chill ...
+          </p>
+          
+          </div>
+      </header>
+    </div>
+  )
+}
+
+export default Header
